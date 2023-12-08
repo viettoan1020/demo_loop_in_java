874c34e670a35ac24dfb19498f782ff6d15d4f19
viettoan1020 <viettoann102003@gmail.com>
2023-12-08 08:59:40 +0700

    presentation

---
+++ loop_in_java/src/For_Loop.java
@@ -0,0 +1,24 @@
+public class For_Loop {
+    public static void main(String[] args) {
+
+
+//        for (int i = 1; i <= 5 ; i++ ) {
+//            System.out.println(i);
+//        }
+
+
+//        int i = 0;
+//        while (i <10) {
+//            System.out.println(i);
+//            i+=2;
+//        }
+
+//        int i = 10;
+//        do {
+//            System.out.println("i is less than 5");
+//        } while (i<5);
+
+    }
+}
+
+
